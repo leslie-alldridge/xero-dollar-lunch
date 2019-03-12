77aec5f5a2512673e5451bf1c8e521e46c7dda42
leslie-alldridge <leslie.alldridge@gmail.com>
2019-03-13 10:08:58 +1300

    3 and 4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const SlackBot = require('slackbots')
+const SlackBot = require("slackbots");
 const PORT = 3000 || process.env.PORT;
 const app = express();
 
@@ -9,32 +9,52 @@ const app = express();
 
 let bot = new SlackBot({
   token: process.env.TOKEN,
-  name: 'appy'
-})
+  name: "appy"
+});
 
 bot.on("start", () => {
-  console.log('started')
-})
+  console.log("started");
+});
 
-bot.on('message', (data) => {
-  console.log(data)
+bot.on("message", data => {
+  console.log(data);
 
-  if (data.type !== "message" && !data.subtype){
-    return
+  if (data.type !== "message" && !data.subtype) {
+    return;
   }
-  handleMessage(data.text, data.user)
-})
+  handleMessage(data.text, data.user);
+});
 
 handleMessage = (message, user) => {
-  console.log(message );
+  console.log(message);
   const params = {
-    icon_emoji: ':cat:'
-  }
-bot.postMessage(user, `you wrote ${message}`, params).then(function(data) {
-    console.log('message sent @@');
+    icon_emoji: ":cat:"
+  };
+  let reply =
+    "Sorry you request wasn't understood. Please enter a dollar value e.g. $3";
+
+  if (message.includes('$3')){
+    bot
+    .postMessage(user, `I've found these lunch ideas: 
+    - $3 - Muffin from BP garage
+    - $3+ - Sandwiches from Cozy Cake Shop
+    `, params)
+    .then(function(data) {
+      console.log("message sent @@");
+    });
     
-})
-}
+  } else if (message.includes('$4')){
+    bot
+    .postMessage(user, `I've found these lunch ideas: 
+    - $4+ - 4 pieces of sushi at One Sushi (BYO container to be more eco)
+    - $4.50 - Rice paper rolls at Cozy Cake Shop
+    `, params)
+    .then(function(data) {
+      console.log("message sent @@");
+    });
+    
+  } 
+};
 
 app.listen(process.env.PORT || PORT, function() {
   console.log("Bot is listening on port " + PORT);
